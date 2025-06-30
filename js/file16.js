59d6e921c037d1a00cc61f77020f7de43e53c71e
aamirhasan1415 <aamirhasan2929@gmail.com>
2025-06-30 09:15:11 +0530

    upadted

--- js/file16.js
+++ js/file16.js
@@ -1,11 +1,46 @@
 //promise
-const f1 = () => {
-  setTimeout(() => {
-    return 5;
-  }, 1000);
+// const f1 = () => {
+//   setTimeout(() => {
+//     return 5;
+//   }, 1000);
+// };
+// const f2 = (x) => {
+//   console.log(x + 6);
+// };
+// const n = f1();
+// f2(n);
+
+// const f1 = () =>
+// {
+//   return new Promise((resolve,reject) =>{
+//     // resolve(5);
+//     reject("Somthing went wrong");
+//   });
+// };
+// const f2 = (x) =>{
+//   console.log(x+6);
+// };
+// // f1().then((n) => f2(n));
+// f1()
+//   .then((n) => f2(n))
+//   .catch((err) => console.log(err));
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) =>res.json())
+  .then((data) =>{
+    data.forEach((value)=> { //data.forEach() = data.map()
+      console.log(value.name +"-" + value.email);
+    });
+  })
+  .catch((err) => console.log(err));
+
+//or
+const fetchdata = async()  =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")   //awat likhna h to funcn must be async
+  const data = await res.json();
+   data.forEach((value)=> { //data.forEach() = data.map()
+   console.log(value.name +"-" + value.email);
+  });
 };
-const f2 = (x) => {
-  console.log(x + 6);
-};
-const n = f1();
-f2(n);
+fetchdata();
